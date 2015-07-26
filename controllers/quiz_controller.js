bd85de42d26abe880df4cbc4302aff801169e492
Jorge Alberto Arocha Muñoz <jorge.arocha@gmail.com>
2015-07-25 23:48:24 -0500

    Busqueda preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,7 +18,13 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function (req, res) {
-	models.Quiz.findAll().then(function (quizes) {
+	var consulta = {};
+	console.log(req.query.search);
+	if (req.query.search) {
+		consulta.where = ["pregunta like ?", req.query.search];
+	}
+	console.log(consulta);
+	models.Quiz.findAll(consulta).then(function (quizes) {
 		res.render('quizes/index.ejs', { quizes: quizes });
 	}).catch(function (error) {
 		next(error);
